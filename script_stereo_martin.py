7ae21d57f4e51bab093ae394ed85fe3968975677
aloeb <ambroise.loeb@inrae.fr>
2024-03-06 17:42:03 +0100

    reconnaissance bords bac par seil de gris, (efficace pour les premiers stades) + def raccourcir image

--- script_stereo_martin.py
+++ script_stereo_martin.py
@@ -1,9 +1,9 @@
 # importation des bibliotheques
 import numpy as np
 import cv2 as cv
-import matplotlib
 from matplotlib import pyplot as plt
-matplotlib.use('TkAgg')  # pour afficher les plt en popup
+
+'''matplotlib.use('TkAgg')  # pour afficher les plt en popup'''
 
 # chargement des parametres stereo
 stereo_path = "/home/loeb/Documents/PycharmProjects/Photogrammetrie_openCV_Mobidiv/calibration"
@@ -29,8 +29,8 @@ else:
     min_disp = MinDisp
 
 # chargement des images gauche et droite
-left_path = ("/home/loeb/Documents/Literal_mobidiv_2023/Session 2023-02-06 "
-             "13-14-02/uplot_100_1/uplot_100_camera_1_2_RGB.jpg")
+left_path = ("/home/loeb/Documents/Literal_mobidiv_2023/Session 2023-06-22 06-32-00"
+             "/uplot_100_1/uplot_100_camera_1_2_RGB.jpg")
 id_image = left_path.split('camera_1')
 right_path = 'camera_2'.join(id_image)
 
@@ -50,12 +50,82 @@ def raccourcir_image(photo):
     return photo
 
 
-# reconaissance des contours du bac
+# reconaissance du bac
 
+# masque des pixels verts
+hsv_img = cv.cvtColor(image_left, cv.COLOR_BGR2HSV)
+lower_green = np.array([10, 20, 20])  # Valeurs minimales de teinte, saturation et valeur pour la couleur verte
+upper_green = np.array([100, 255, 255])  # Valeurs maximales de teinte, saturation et valeur pour la couleur verte
+mask_green = cv.inRange(hsv_img, lower_green, upper_green)
+img_without_green = cv.bitwise_and(image_left, image_left, mask=~mask_green)  # Appliquer le masque
+img_gray = cv.cvtColor(img_without_green, cv.COLOR_BGR2GRAY)  # Convertir en niveaux de gris
+
+# seuil de gris
+
+mean_intensity = np.mean(img_gray)
+_, thresholded_img = cv.threshold(img_gray, 1.5*mean_intensity, 255, cv.THRESH_BINARY)
+# thresholded_img = cv.adaptiveThreshold(img_gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 511, C = -mean_intensity)
+plt.figure() and plt.imshow(thresholded_img)
+
+# Recherche des bords de bac
+mean_center = np.mean(thresholded_img[1200:2400, 1500:3000])
+mean_tot = np.mean(thresholded_img)
+height, width = thresholded_img.shape
+centre_colonne = width // 2
+centre_ligne = height // 2
+
+for colonne in range(centre_colonne, width):
+    if np.mean(thresholded_img[1200:2600:, colonne]) > mean_tot:
+        break
+nouvelle_largeur_droite = colonne
+
+for colonne in range(centre_colonne, -1, -1):
+    if np.mean(thresholded_img[1200:2600:, colonne]) > mean_tot:
+        break
+nouvelle_largeur_gauche = colonne
+
+for ligne in range(centre_ligne, height):
+    if np.mean(thresholded_img[ligne, 1500:3000]) > mean_tot:
+        break
+nouvelle_hauteur_bas = ligne
+
+for ligne in range(centre_ligne, -1, -1):
+    if np.mean(thresholded_img[ligne, 1500:3000]) > mean_tot:
+        break
+nouvelle_hauteur_haut = ligne
+
+
+# Extraire la région du bac de l'image seuillée
+zeros_img = np.zeros_like(thresholded_img)
+image_l = cv.cvtColor(zeros_img, cv.COLOR_GRAY2BGR)
+image_r = cv.cvtColor(zeros_img, cv.COLOR_GRAY2BGR)
+image_l[nouvelle_hauteur_haut:nouvelle_hauteur_bas, nouvelle_largeur_gauche:nouvelle_largeur_droite] = image_left[nouvelle_hauteur_haut:nouvelle_hauteur_bas, nouvelle_largeur_gauche:nouvelle_largeur_droite]
+image_r[nouvelle_hauteur_haut:nouvelle_hauteur_bas, nouvelle_largeur_gauche:nouvelle_largeur_droite] = image_right[nouvelle_hauteur_haut:nouvelle_hauteur_bas, nouvelle_largeur_gauche:nouvelle_largeur_droite]
+plt.figure() and plt.imshow(image_l)
+
+
+
+'''
+# blurred = cv.GaussianBlur(thresholded_img, (5, 5), 0)
+# Détection des contours
+edges = cv.Canny(thresholded_img, 0, 0, L2gradient=True)
+
+# Détection des lignes à l'aide de la transformée de Hough
+lines = cv.HoughLinesP(thresholded_img, 1, np.pi / 180, threshold=0, minLineLength=500, maxLineGap=20)
+
+# Dessiner les lignes détectées sur une copie de l'image originale
+lines_img = image_left.copy()
+if lines is not None:
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv.line(lines_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+plt.figure() and plt.imshow(lines_img)
+'''
 
 # conversion en niveaux de gris
-img_l = cv.cvtColor(image_left, cv.IMREAD_GRAYSCALE + cv.IMREAD_IGNORE_ORIENTATION)
-img_r = cv.cvtColor(image_right, cv.IMREAD_GRAYSCALE + cv.IMREAD_IGNORE_ORIENTATION)
+# img_l = cv.cvtColor(image_left, cv.COLOR_BGR2GRAY)
+img_l = cv.cvtColor(image_l, cv.IMREAD_GRAYSCALE + cv.IMREAD_IGNORE_ORIENTATION)
+img_r = cv.cvtColor(image_r, cv.IMREAD_GRAYSCALE + cv.IMREAD_IGNORE_ORIENTATION)
 rgb_l = cv.cvtColor(image_left, cv.COLOR_BGR2RGB)
 
 # rectification des images (transformation de perspective)
@@ -108,7 +178,6 @@ mask_distance = z_image > 1400  # Masque en fonction de la distance
 z_image[mask_distance] = np.nan
 mask_distance2 = z_image < 600
 z_image[mask_distance2] = np.nan
-
 
 # racourcir image
 z_image = raccourcir_image(z_image)
