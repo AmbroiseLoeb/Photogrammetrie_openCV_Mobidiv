0370f5f61fcc258fda2c639a9bc65e1c0f964587
aloeb <ambroise.loeb@inrae.fr>
2024-04-30 14:18:50 +0200

    update bord_bac et contour bac, ajout de lignes verticales et horizontales comblant les vides dans les rebords du bac

--- script_stereo_martin.py
+++ script_stereo_martin.py
@@ -1,4 +1,5 @@
 # importation des bibliotheques
+from pathlib import Path
 import os
 import math
 import numpy as np
@@ -7,6 +8,7 @@ from matplotlib import pyplot as plt
 import scipy.ndimage as ndi
 import csv
 from itertools import zip_longest
+from tqdm import tqdm
 
 
 def raccourcir_image(image):
@@ -21,101 +23,156 @@ def raccourcir_image(image):
     return photo
 
 
-def bord_bac(image):
-    """ definir les contour du bac sur la photo, detecter et supprimer le capteur si besoin """
-
-    # convertir en rgba (a = transparence)
-    img_sans_capteur = cv.cvtColor(image, cv.COLOR_RGB2RGBA)
-
-    # definir la colone et la ligne centrale
-    height, width, color = img_sans_capteur.shape
-    centre_colonne = width // 2
-    centre_ligne = height // 2
-
-    # masque des pixels verts
-    hsv_img = cv.cvtColor(img_sans_capteur, cv.COLOR_BGR2HSV)
-    lower_green = np.array([10, 20, 20])  # Valeurs min de teinte, saturation et valeur pour la couleur verte
-    upper_green = np.array([100, 255, 255])  # Valeurs max de teinte, saturation et valeur pour la couleur verte
-    mask_green = cv.inRange(hsv_img, lower_green, upper_green)
-    img_without_green = cv.bitwise_and(img_sans_capteur, img_sans_capteur, mask=~mask_green)  # Appliquer le masque
-    img_gray = cv.cvtColor(img_without_green, cv.COLOR_BGR2GRAY)  # Convertir en niveaux de gris
-
-    # seuil de gris
-    seuil_gris = max(np.mean(img_gray), 20)
-    _, thresholded_img = cv.threshold(img_gray, seuil_gris, 255, cv.THRESH_BINARY)
-    # plt.figure() and plt.imshow(thresholded_img)
-
-    # Étiqueter les objets, calculer leur coordonnées et leur taille
-    labels, nb_labels = ndi.label(thresholded_img)
-    coordinates = ndi.center_of_mass(thresholded_img, labels, range(nb_labels + 1))
-    sizes = ndi.sum(thresholded_img, labels, range(nb_labels + 1))
-    # Supprimer les objets inférieurs à 300 pixels et le capteur
-    seuil2 = 0
-
-    filtered_image = np.zeros_like(thresholded_img)
-    for label in range(1, nb_labels + 1):
-        if 1200 <= coordinates[label][0] <= 2500 and 1500 <= coordinates[label][1] <= 3200:  # capteur
-            if 20000 * 255 <= sizes[label] <= 80000 * 255:
-                img_sans_capteur[labels == label] = (0, 0, 0, 0)
-                seuil2 = 100000  # augmentation du seuil en cas de présence du capteur
-                filtered_image[labels == label] = 255
-            elif sizes[label] >= 5000 * 255:
-                filtered_image[labels == label] = 255
-        elif sizes[label] >= 300 * 255:
-            filtered_image[labels == label] = 255
-    # plt.figure() and plt.imshow(filtered_image)
-
-    # filtered_image = thresholded_img
-    # paramettres pour recherche des bords de bac
-    largueur_min = 1000*0
-    longueur_min = 1200*0
-    nouvelle_longueur = 0
-    nouvelle_largeur = 0
-    seuil_bordure = 20000
-    nouvelle_largeur_haut = centre_ligne
-    nouvelle_largeur_bas = centre_ligne
-    nouvelle_longueur_gauche = centre_colonne
-    nouvelle_longueur_droite = centre_colonne
-    n = 0
-
-    # Recherche des bords de bac
-    while nouvelle_longueur <= longueur_min or nouvelle_largeur <= largueur_min and n <= 2:
-        if nouvelle_largeur <= largueur_min:
-            colonne = centre_colonne
-            for colonne in range(centre_colonne, width):
-                if np.sum(filtered_image[1200:2500, colonne]) > max(seuil_bordure, seuil2):
-                    break
-            nouvelle_longueur_droite = colonne
-
-            for colonne in range(centre_colonne, -1, -1):
-                if np.sum(filtered_image[1200:2500, colonne]) > max(seuil_bordure, seuil2):
-                    break
-            nouvelle_longueur_gauche = colonne
-            nouvelle_largeur = nouvelle_longueur_droite - nouvelle_longueur_gauche
-
-        if nouvelle_longueur <= longueur_min:
-            ligne = centre_ligne
-            for ligne in range(centre_ligne, height):
-                if np.sum(filtered_image[ligne, min(1800, nouvelle_longueur_gauche):min(3200, nouvelle_longueur_droite)]) > max(seuil_bordure, seuil2):
-                    break
-            nouvelle_largeur_bas = ligne
-
-            for ligne in range(centre_ligne - 100, -1, -1):
-                if np.sum(filtered_image[ligne, min(1800, nouvelle_longueur_gauche):min(3200, nouvelle_longueur_droite)]) > max(seuil_bordure, seuil2):
-                    break
-            nouvelle_largeur_haut = ligne
-            nouvelle_longueur = nouvelle_largeur_bas - nouvelle_largeur_haut
-
-        filtered_image[nouvelle_largeur_haut:nouvelle_largeur_bas, nouvelle_longueur_gauche:nouvelle_longueur_droite] = 0
-        n = n+1
-
-    # plt.figure() and plt.imshow(filtered_image)
-    return (nouvelle_largeur_haut, nouvelle_largeur_bas, nouvelle_longueur_gauche, nouvelle_longueur_droite,
-            img_sans_capteur)
-
-
 def contour_bac(image1, image2):
     """ definir les contour finaux du bac, a partir des contours de bac de deux photos """
+    def bord_bac(image, seuil_small_obj=300):
+        """ definir les contour du bac sur la photo """
+
+        def label_objects(image):
+            # Étiqueter les objets, calculer leur coordonnées et leur taille
+            labels_, nb_labels_ = ndi.label(image)
+            coordinates_ = np.array(ndi.center_of_mass(image, labels_, range(1, nb_labels_ + 1)))
+            sizes_ = ndi.sum(image, labels_, range(nb_labels_ + 1))
+            return labels_, nb_labels_, coordinates_, sizes_
+
+        height, width = image.shape[:2]
+
+        # masque des pixels verts
+        hsv_img = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+        lower_green = np.array([10, 20, 20])  # Valeurs min de teinte, saturation et valeur pour la couleur verte
+        upper_green = np.array([100, 255, 255])  # Valeurs max de teinte, saturation et valeur pour la couleur verte
+        mask_green = cv.inRange(hsv_img, lower_green, upper_green)
+        img_without_green = cv.bitwise_and(image, image, mask=~mask_green)  # Appliquer le masque
+        # plt.figure() and plt.imshow(img_without_green)
+
+        # masque des pixels non gris, seuil de gris
+        img_gray = cv.cvtColor(img_without_green, cv.COLOR_BGR2GRAY)
+        seuil_gris = max(np.mean(img_gray), 20)
+        _, image_filtree = cv.threshold(img_gray, seuil_gris, 255, cv.THRESH_BINARY)
+        # plt.figure() and plt.imshow(image_filtree)
+
+        # Supprimer les petits objets
+        labels, nb_labels, coordinates, sizes = label_objects(image_filtree)
+        image_filtree2 = np.zeros_like(image_filtree)
+        for label in (range(1, nb_labels + 1)):
+            if sizes[label] >= seuil_small_obj * 255:
+                image_filtree2[labels == label] = 255
+        # plt.figure() and plt.imshow(image_filtree2)
+
+        # Supprimer le capteur (même si en plusieurs morceaux)
+        labels, nb_labels, coordinates, sizes = label_objects(image_filtree2)
+        image_filtree3 = image_filtree2.copy()
+        for i in (range(len(coordinates))):
+            if 1200 <= coordinates[i][0] <= 2100 and 1800 <= coordinates[i][1] <= 2900:
+                for j in range(i + 1, len(coordinates)):
+                    if (abs(coordinates[i][0] - coordinates[j][0]) <= 300) and (
+                            abs(coordinates[i][1] - coordinates[j][1]) <= 300):
+                        # print(coordinates[i])
+                        # print(coordinates[j])
+                        # print(sizes[i])
+                        # print(sizes[j])
+                        if 2000 * 255 <= sizes[i] + sizes[j] <= 200000 * 255:  # capteur
+                            # print(sizes[i] + sizes[j])
+                            image_filtree3[labels == i + 1] = 0
+                            image_filtree3[labels == j + 1] = 0
+                            image_filtree3[int(coordinates[i][0] - 300): int(coordinates[i][0] + 300),
+                            int(coordinates[i][1] - 300): int(coordinates[i][1] + 300)] = 0
+                            image_filtree3[int(coordinates[j][0] - 300): int(coordinates[j][0] + 300),
+                            int(coordinates[j][1] - 300): int(coordinates[j][1] + 300)] = 0
+                    elif 2000 * 255 <= sizes[i] <= 200000 * 255:  # capteur
+                        # print(sizes[i])
+                        image_filtree3[labels == i + 1] = 0
+
+        # Tracer des lignes entre les objets ayant les mêmes coordonnées horizontales
+        image_with_lines = image_filtree3.copy()
+        labels, nb_labels, coordinates, sizes = label_objects(image_filtree3)
+        for i in (range(len(coordinates))):
+            for j in range(i + 1, len(coordinates)):
+                if abs(coordinates[i][0] - coordinates[j][0]) <= 100 and abs(
+                        coordinates[i][1] - coordinates[j][1]) <= 1000:
+                    cv.line(image_with_lines, (int(coordinates[i][1]), int(coordinates[i][0])),
+                            (int(coordinates[j][1]), int(coordinates[j][0])), (255, 255, 255), 30)
+            # Tracer une ligne si l'objet est proche du bord gauche ou droit de l'image
+            '''
+            if coordinates[i][1] <= 1500:
+                cv.line(image_with_lines, (int(coordinates[i][1]), int(coordinates[i][0])), (0, int(coordinates[i][0])),
+                        (255, 255, 255), 15)
+            elif coordinates[i][1] >= width - 1500:
+                cv.line(image_with_lines, (int(coordinates[i][1]), int(coordinates[i][0])),
+                        (image_with_lines.shape[1] - 1, int(coordinates[i][0])), (255, 255, 255), 15)
+            '''
+
+        # Tracer des lignes entre les objets ayant les mêmes coordonnées verticales
+        for i in (range(len(coordinates))):
+            for j in range(i + 1, len(coordinates)):
+                if abs(coordinates[i][1] - coordinates[j][1]) <= 100 and abs(
+                        coordinates[i][0] - coordinates[j][0]) <= 1500:
+                    cv.line(image_with_lines, (int(coordinates[i][1]), int(coordinates[i][0])),
+                            (int(coordinates[j][1]), int(coordinates[j][0])), (255, 255, 255), 30)
+                    # Tracer une ligne si l'objet est proche du bord supérieur ou inférieur de l'image
+            '''
+                if coordinates[i][0] <= 1500:
+                    cv.line(image_with_lines, (int(coordinates[i][1]), int(coordinates[i][0])), (int(coordinates[i][1]), 0),
+                            (255, 255, 255), 15)
+                elif coordinates[i][0] >= image_with_lines.shape[0] - 1500:
+                    cv.line(image_with_lines, (int(coordinates[i][1]), int(coordinates[i][0])),
+                            (int(coordinates[i][1]), image_with_lines.shape[0] - 1), (255, 255, 255), 15)
+            '''
+        # plt.figure() and plt.imshow(image_with_lines)
+
+        # paramettres pour la recherche des bords de bac
+        largueur_min = 1600
+        longueur_min = 1800
+        nouvelle_longueur = 0
+        nouvelle_largeur = 0
+        seuil_bordure = 300 * 255
+        centre_ligne = height // 2
+        centre_colonne = width // 2
+        nouvelle_largeur_haut = centre_ligne
+        nouvelle_largeur_bas = centre_ligne
+        nouvelle_longueur_gauche = centre_colonne
+        nouvelle_longueur_droite = centre_colonne
+
+        # Recherche des bords du bac
+        image_with_lines = image_with_lines + image_filtree2
+        while (nouvelle_longueur <= longueur_min) or (nouvelle_largeur <= largueur_min):
+            if nouvelle_longueur <= longueur_min:
+                ligne = centre_ligne
+                for ligne in range(centre_ligne, height):
+                    if np.sum(image_with_lines[ligne, 1800:2900]) > seuil_bordure:
+                        # print(np.sum(image_with_lines[ligne, 1800:2900]))
+                        break
+                nouvelle_largeur_bas = ligne
+
+                for ligne in range(centre_ligne, 0, -1):
+                    if np.sum(image_with_lines[ligne, 1800:2900]) > seuil_bordure:
+                        # print(np.sum(image_with_lines[ligne, 1800:2900]))
+                        break
+                nouvelle_largeur_haut = ligne
+                nouvelle_longueur = nouvelle_largeur_bas - nouvelle_largeur_haut
+
+            if nouvelle_largeur <= largueur_min:
+                colonne = centre_colonne
+                for colonne in range(centre_colonne, width):
+                    if np.sum(image_with_lines[1200:2100, colonne]) > seuil_bordure:
+                        # print(np.sum(image_with_lines[1200:2100, colonne]))
+                        break
+                nouvelle_longueur_droite = colonne
+
+                for colonne in range(centre_colonne, 0, -1):
+                    if np.sum(image_with_lines[1200:2100, colonne]) > seuil_bordure:
+                        # print(np.sum(image_with_lines[1200:2100, colonne]))
+                        break
+                nouvelle_longueur_gauche = colonne
+                nouvelle_largeur = nouvelle_longueur_droite - nouvelle_longueur_gauche
+
+            # image_with_lines[nouvelle_largeur_haut:nouvelle_largeur_bas, 1800:2900] = 0
+            # image_with_lines[1200:2100, nouvelle_longueur_gauche:nouvelle_longueur_droite] = 0
+            image_with_lines[nouvelle_largeur_haut:nouvelle_largeur_bas,
+            nouvelle_longueur_gauche:nouvelle_longueur_droite] = 0
+
+        # plt.figure() and plt.imshow(image_with_lines)
+        return nouvelle_largeur_haut, nouvelle_largeur_bas, nouvelle_longueur_gauche, nouvelle_longueur_droite
 
     # definir la colone et la ligne centrale
     height, width, color = image1.shape
@@ -123,8 +180,8 @@ def contour_bac(image1, image2):
     centre_ligne = height // 2
 
     # charger les contours de bac des images 1 et 2
-    haut_left, bas_left, gauche_left, droite_left = bord_bac(image1)[:-1]
-    haut_right, bas_right, gauche_right, droite_right = bord_bac(image2)[:-1]
+    haut_left, bas_left, gauche_left, droite_left = bord_bac(image1)
+    haut_right, bas_right, gauche_right, droite_right = bord_bac(image2)
 
     # definir les nouveaux contours du bac
     '''
@@ -185,12 +242,10 @@ def carte_profondeur(image1, image2):
     # conversion en niveaux de gris
     img_l = cv.cvtColor(image1, cv.IMREAD_GRAYSCALE + cv.IMREAD_IGNORE_ORIENTATION)
     img_r = cv.cvtColor(image2, cv.IMREAD_GRAYSCALE + cv.IMREAD_IGNORE_ORIENTATION)
-    # rgb_l = cv.cvtColor(image1, cv.COLOR_BGR2RGB)
 
     # rectification des images (transformation de perspective)
     imglCalRect = cv.remap(img_l, mapx11, mapx12, cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
     imgrCalRect = cv.remap(img_r, mapx21, mapx22, cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
-    # rgblCalRect = cv.remap(rgb_l, mapx11, mapx12, cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
 
     # reduction taille des images
     h_ori, w_ori = imglCalRect.shape
@@ -231,12 +286,12 @@ def carte_profondeur(image1, image2):
     # affichage de la carte de profondeur
     '''
     plt.figure()
-    plt.imshow(z_image, cmap='jet', vmin=800, vmax=1500)
+    plt.imshow(z_image, cmap='jet', vmin=800, vmax=2000)
     '''
     # calcul et affichage de la carte de profondeur 2
     '''
      Z = abs(FL * B / disparity)
-     plt.figure() and plt.imshow(Z, cmap='jet', vmin=800, vmax=1500)
+     plt.figure() and plt.imshow(Z, cmap='jet', vmin=800, vmax=2000)
     '''
 
     return z_image
@@ -327,20 +382,20 @@ def hauteur_locale(matrice, nombre_zones):
 
 
 # PATH
-PATH = "/home/loeb/Documents/Comparaison_mesures"
+PATH = "/home/loeb/Documents/Comparaison_mesures (copie)"
 n_zones = 100
 print('nombre de zones =', n_zones)
 csv_path = PATH + "/" + "hauteurs_opencv" + str(n_zones) + ".csv"
 sessionlist = os.listdir(PATH)
-for session in sorted(sessionlist):
+for session in tqdm(sorted(sessionlist)):
     if session.find("Session") == 0:
         print(session)
         plotlist = os.listdir(PATH + "/" + session)
         if not os.path.exists(PATH + "/" + session + "/" + "mask_z_map"):
             # Crée le fichier s'il n'existe pas
             os.makedirs(PATH + "/" + session + "/" + "mask_z_map")
-        for plot in sorted(plotlist):
-            if plot.find("uplot_7_1") == 0:
+        for plot in tqdm(sorted(plotlist)):
+            if plot.find("uplot") == 0:
                 print(plot)
                 imglist = os.listdir(PATH + "/" + session + "/" + plot)
                 for file in imglist:
@@ -356,9 +411,7 @@ for session in sorted(sessionlist):
                         # plt.figure() and plt.imshow(image_left)
 
                         # carte de profondeur, avec suppression du capteur
-                        depth_image1 = carte_profondeur(bord_bac(image_left)[4], image_right)
-                        depth_image2 = carte_profondeur(image_left, bord_bac(image_right)[4])
-                        depth_image = (depth_image1 * depth_image2) / depth_image1
+                        depth_image = carte_profondeur(image_left, image_right)
                         # plt.figure() and plt.imshow(depth_image, cmap='jet', vmin=800, vmax=1500)
 
                         # Extraire la region du bac
